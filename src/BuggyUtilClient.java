eb45253479f9453dcefa74ce9fc2aee36fb5757b
nivink <nivin1999@gmail.com>
2018-10-02 15:13:39 -0400

    Adding a commment

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,9 +3,10 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
+        // prints out a statement
         System.out.println("In: first = " + "\"Sean\"" +
                 ", last = " + "\"Flannery\"");
-        System.out.println("Out: " +
+        System.out.println("Out: " +);
         StringUtils.makePurdueUsername("Sean", "Flannery"));
     }
 }
